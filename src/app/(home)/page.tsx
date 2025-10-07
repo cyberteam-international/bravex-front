ef8621c37798819a81651d585248b1995f493792
Donatello <daniilbortvin@gmail.com>
2025-10-07 07:01:07 +0700

    added about section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/sections/HeroSection/HeroSection";
+import AboutSection from "@/components/sections/AboutSection/AboutSection";
 
 
   
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <AboutSection />
     </>
     
   );
---
+++ src/components/sections/AboutSection/AboutSection.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+import styles from './AboutSection.module.css';
+import Logo from '@/assets/icons/logo.svg';
+import AboutSectionBackground from '@/assets/about.webp';
+
+const AboutSection: React.FC = () => {
+  return (
+    <div className="container-max">
+      <div className={styles.aboutInner}>
+        <div className={styles.aboutImageWrap}>
+          <Image
+            className={`${styles.aboutImageLogo} fade-in`}
+            src={Logo}
+            alt=""
+            width={158}
+            height={158}
+          />
+          <Image
+            className={styles.aboutImageItem}
+            src={AboutSectionBackground}
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+
+        <div className={styles.aboutContent}>
+          <h2 className={`${styles.aboutContentHeader} fade-in`}>
+            WHAT IS <span className={styles.aboutContentHeaderText}>STEEL-LIGHT-CONCRETE SYSTEM</span>
+          </h2>
+          <p className={`${styles.aboutContentText} fade-in`}>
+            Steel-Light-Concrete is a modern construction technology that combines a strong galvanized steel frame with lightweight foam concrete, creating warm, durable, and energy-efficient buildings. The steel frame provides exceptional load-bearing capacity and long-term stability, while the monolithic foam concrete core delivers excellent thermal and acoustic insulation.
+          </p>
+
+
+          <div className={`${styles.headerButton} fade-in`}>
+            <span className={styles.headerButtonText}>Get free Estimate</span>
+            <img 
+              src="/assets/icons/white-arrow-svg.svg" 
+              alt="" 
+              className={styles.headerButtonArrow} 
+            />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
