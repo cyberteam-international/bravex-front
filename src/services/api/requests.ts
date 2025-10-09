7c12c9658cae4a25e1465068ad5e34d5d2065469
Donatello <daniilbortvin@gmail.com>
2025-10-09 14:49:28 +0700

    Сделал роутинг для обычных страниц по slug

--- src/services/api/requests.ts
+++ src/services/api/requests.ts
@@ -5,7 +5,11 @@ export const BASE_BACK_URL = 'http://localhost:1337';
 export const BASE_API_URL = 'http://localhost:1337/api';
 
 // Создаем инстанс axios для API
-export const getPageData = (pageName: string) => axios({
+export const getHomePageData = (pageName: string) => axios({
   baseURL: `${BASE_API_URL}/${pageName}/full`,
+});
+
+export const getPageData = (pageName: string) => axios({
+  baseURL: `${BASE_API_URL}/${pageName}`,
 });
 
