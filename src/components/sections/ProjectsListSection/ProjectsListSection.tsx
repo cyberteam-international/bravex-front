3d554fe211d380f929f2f214fa489efd63f60af4
Donatello <daniilbortvin@gmail.com>
2025-10-18 19:55:24 +0700

    Убрал ссылки из проектов

--- src/components/sections/ProjectsListSection/ProjectsListSection.tsx
+++ src/components/sections/ProjectsListSection/ProjectsListSection.tsx
@@ -79,9 +79,13 @@ const ProjectsListSection = ({ data }: SectionProps) => {
                     {categories.length > 0 && (
                       <div className={styles.projectCategories}>
                         {categories.map((category: any, index: number) => (
-                          <Link href={`/projects?category=${category.id}`} key={category.id} className={styles.categoryBadge}>
+                          // <Link href={`/projects?category=${category.id}`} key={category.id} className={styles.categoryBadge}>
+                          //   {category.Name}
+                          // </Link>
+
+                          <span key={category.id} className={styles.categoryBadge}>
                             {category.Name}
-                          </Link>
+                          </span>
                         ))}
                       </div>
                     )}
@@ -92,12 +96,12 @@ const ProjectsListSection = ({ data }: SectionProps) => {
                         <div className={styles.projectButtonContent}>
                           <h3 className={styles.projectTitle}>{project.Title}</h3>
                           <p className={styles.projectDescription}>{project.Description}</p>
-                          <Link href={`/projects/${project.slug}`} className={styles.customLearnMoreButton}>
+                          {/* <Link href={`/projects/${project.slug}`} className={styles.customLearnMoreButton}>
                             <span>Learn More</span>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
-                          </Link>
+                          </Link> */}
                         </div>
                       </div>
                     </div>
