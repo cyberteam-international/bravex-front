1864907d4568ccee1bf467a50b151d3d20887160
Donatello <daniilbortvin@gmail.com>
2025-11-13 14:53:34 +0700

    refactor: improve code formatting and consistency in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,26 +4,35 @@ import Logo from "@/assets/icons/logo.svg";
 import styles from "./Header.module.css";
 import HeaderMenu from "./HeaderMenu";
 
-const Header = () => {  
+const Header = () => {
   return (
     <header className={styles.header}>
       <Link href="/">
-        <Image src={Logo} width={120} height={35} alt="" className={styles.logo} priority />
+        <Image
+          src={Logo}
+          width={120}
+          height={35}
+          alt=""
+          className={styles.logo}
+          priority
+        />
       </Link>
-      <HeaderMenu />
-        
-        <div className={styles.header_right}>
-          <Link href="#technology" className={styles['header-button-catalog']}>Catalog</Link>
-          <Link className={`${styles['burger-menu']}`} href="#mobile-menu">
-              <div className={styles['burger-btn']}>
-                  <span className={styles['burger-line']}></span>
-                  <span className={styles['burger-line']}></span>
-                  <span className={styles['burger-line']}></span>
-              </div>           
-          </Link>
-        </div>
+      {/* <HeaderMenu /> */}
+
+      <div className={styles.header_right}>
+        <Link href="#technology" className={styles["header-button-catalog"]}>
+          Catalog
+        </Link>
+        <Link className={`${styles["burger-menu"]}`} href="#mobile-menu">
+          <div className={styles["burger-btn"]}>
+            <span className={styles["burger-line"]}></span>
+            <span className={styles["burger-line"]}></span>
+            <span className={styles["burger-line"]}></span>
+          </div>
+        </Link>
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
